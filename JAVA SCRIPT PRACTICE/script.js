ff95762c73d67853b29a033d76c2f181f0eb5ec8
Aimanmirza2007 <sm501623@gmail.com>
2024-09-18 16:11:53 +0500

    Add files via upload

---
+++ JAVA SCRIPT PRACTICE/script.js
@@ -0,0 +1,50 @@
+// // Question no 1
+alert("My name is Aiman")
+
+// //Question no 2
+  var meassage = "Thanks for the Visited";
+ var username = "Aiman Mirza";
+ console.log(username)
+ alert( meassage  +" "+ username);
+  console.log(` ${meassage}  ${username}`)
+// var myName = ("urooj shakeel")
+// alert("hello, my name is" +" "+ myName);
+// console.log(myName);
+
+
+
+// // Question no 3
+var a = 4; 
+var b = 2;
+
+console.log(a++ + --b - --a + b++ + a + b);
+
+//Question no 4
+var college = prompt("In which college you want to take addmission")
+var option = prompt("Which field you like to choose")
+var percentage = prompt("What was your percentage in previous year?")
+if (
+    college === "PCHS" &&
+    option === "Computer science", "Bio", "Pre enginerring" &&
+    percentage >= 70
+) {
+    alert("your addmission wil be done in this college")
+}
+else if (
+    percentage < 70
+) {
+    alert("your addmission wil be  not done in this college")
+}
+else (
+    alert("your not Qualified")
+)
+
+
+
+
+
+
+
+
+
+
